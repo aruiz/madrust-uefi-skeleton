99c527c02b52c0f1ca0386e5808ca2fcbec35fa1
Alberto Ruiz <aruiz@gnome.org>
2019-03-27 21:44:01 +0000

    Use latest entry point name and arg type

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,15 @@
 extern crate uefi;
 extern crate uefi_services;
 
-use uefi::table::{SystemTable, Boot};
+#[macro_use]
+extern crate log;
+
+use uefi::table::{SystemTable, Boot, Runtime};
+use core::fmt::Write;
 
 #[no_mangle]
-pub extern "win64" fn uefi_start(_image_handle: uefi::Handle, system_table: &'static SystemTable<Boot>) -> ! {
+pub extern "win64" fn efi_main(_image_handle: uefi::Handle, system_table: SystemTable<Boot>) -> ! {
+    let rev = system_table.uefi_revision();
+    info!("UEFI {}.{}", rev.major(), rev.minor());
     loop {}
 }
