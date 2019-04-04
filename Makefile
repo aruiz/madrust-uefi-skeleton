d52794573a4bee08cfa32ebeb4901ba81d1018d0
Alberto Ruiz <aruiz@gnome.org>
2019-04-04 14:09:36 +0100

    Soporte para leer ficheros

--- Makefile
+++ Makefile
@@ -11,4 +11,4 @@ run-qemu:
 	mkdir -p efi/EFI/BOOT/
 	cp target/x86_64-unknown-uefi/debug/uefi-app.efi efi/EFI/BOOT/BOOTX64.EFI
 	echo "\\EFI\\BOOT\\BOOTX64.EFI" > efi/EFI/BOOT/startup.nsh
-	qemu-kvm -nodefaults -vga std -machine q35,accel=kvm:tcg -m 128M -drive if=pflash,format=raw,readonly,file=OVMF_CODE.fd -drive if=pflash,format=raw,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:efi/ -serial stdio -monitor vc:1024x768
+	qemu-kvm -nodefaults -vga std -machine q35,accel=kvm:tcg -m 128M -drive if=pflash,format=raw,readonly,file=/usr/share/OVMF/OVMF_CODE.fd -drive if=pflash,format=raw,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:efi/ -serial stdio -monitor vc:1024x768
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,75 @@
 #![no_std]
 #![no_main]
 
+#![feature(alloc)]
+
+extern crate alloc;
+
 extern crate uefi;
+extern crate uefi_alloc;
 extern crate uefi_services;
 
-#[macro_use]
-extern crate log;
+use alloc::string::String;
+use alloc::vec::Vec;
 
-use uefi::table::{SystemTable, Boot, Runtime};
+use uefi::table::{SystemTable, Boot};
+use uefi::proto::media::{
+    fs::SimpleFileSystem,
+    file::*,
+};
+use uefi::data_types::Align;
+use uefi::table::boot::{
+    MemoryDescriptor,
+    SearchType,
+};
+
 use core::fmt::Write;
 
 #[no_mangle]
-pub extern "win64" fn efi_main(_image_handle: uefi::Handle, system_table: SystemTable<Boot>) -> ! {
-    let rev = system_table.uefi_revision();
-    info!("UEFI {}.{}", rev.major(), rev.minor());
+pub extern "win64" fn efi_main(_handle: uefi::Handle, system_table: SystemTable<Boot>)  {
+    unsafe{ uefi_alloc::init(system_table.boot_services()) }  ;
+
+    let size = system_table
+        .boot_services()
+        .locate_handle(SearchType::from_proto::<SimpleFileSystem>(), None).expect("Could not locate handles")
+        .unwrap();
+
+    assert!(size > 0);
+
+    let mut buffer = Vec::with_capacity(size);
+
+    unsafe { buffer.set_len(size); }
+
+    system_table.boot_services()
+        .locate_handle(SearchType::from_proto::<SimpleFileSystem>(), Some(&mut buffer[..])).expect("Could not locate handle")
+        .unwrap();
+
+    let fs_handle = buffer.first().unwrap();
+
+    let fs = system_table.boot_services()
+        .handle_protocol::<SimpleFileSystem>(*fs_handle).expect("Could not handle protocol")
+        .unwrap()
+        .get();
+
+    let mut root = unsafe { (*fs).open_volume().expect("Could not open volume").unwrap() };
+
+    let mut buf = Vec::<u8>::with_capacity(FileInfo::alignment() * 50);
+    let root_file = loop {
+
+        match root.read_entry(&mut buf[..]).map_err(|err| err.split()) {
+            Ok(ret) => break ret.log().unwrap(),
+            Err((_, Some(new_size))) => {
+                buf.extend((0..new_size - buf.len()).map(|_| 0));
+            }
+            Err((status, None)) => panic!("Can't read root dir status: {:?}", status),
+        };
+    };
+
+
+
+    system_table.stdout().write_str("Ficheros en la particion EFI: ");
+    system_table.stdout().write_str(String::from_utf16_lossy(root_file.file_name().to_u16_slice()).as_str());
+    system_table.stdout().write_str("\n");
+
     loop {}
 }
