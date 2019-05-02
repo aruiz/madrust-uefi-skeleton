3be395ad4931a32b7978c4400cfe4351c7b6502b
Alberto Ruiz <aruiz@gnome.org>
2019-05-02 23:03:15 +0100

    Leer todos los ficheros y no solo la raiz

--- src/main.rs
+++ src/main.rs
@@ -24,9 +24,53 @@ use uefi::table::boot::{
 
 use core::fmt::Write;
 
+fn crawl_tree (system_table: &SystemTable<Boot>, dir: &mut Directory, depth: u8) {
+  let mut buf = Vec::<u8>::with_capacity(FileInfo::alignment() * 50);
+  loop {
+    match dir.read_entry(&mut buf[..]).map_err(|err| err.split()) {
+      Ok(ret) => {
+        if let Some(f) = ret.log() {
+          let filename = String::from_utf16_lossy(f.file_name().to_u16_slice());
+          if filename.as_str() == "." || filename.as_str() == ".." {
+            continue;
+          }
+
+          for _ in 0..depth {
+            let _ = system_table.stdout().write_str("    ");
+          };
+          let _ = system_table.stdout().write_str(filename.as_str());
+
+          match dir.handle().open(filename.as_str(), FileMode::Read, FileAttribute::READ_ONLY) {
+            Ok(f) => {
+              if let Ok(c) = f.log().into_type() {
+                match c.log() {
+                  FileType::Dir(mut d) => {
+                    let _ = system_table.stdout().write_str("/\n");
+                    crawl_tree(system_table, &mut d, depth+1);
+                  }
+                  FileType::Regular(_) => {
+                    let _ = system_table.stdout().write_str("\n");
+                  }
+                }
+              }
+            }
+            Err(_) => {}
+          }
+        } else {
+          break;
+        }
+      }
+      Err((_, Some(new_size))) => {
+        buf.extend((0..new_size - buf.len()).map(|_| 0));
+      }
+      Err((status, None)) => panic!("Can't read root dir status: {:?}", status),
+    };
+  };
+}
+
 #[no_mangle]
 pub extern "win64" fn efi_main(_handle: uefi::Handle, system_table: SystemTable<Boot>)  {
-    unsafe{ uefi_alloc::init(system_table.boot_services()) }  ;
+    unsafe{ uefi_alloc::init(system_table.boot_services()) };
 
     let size = system_table
         .boot_services()
@@ -50,22 +94,10 @@ pub extern "win64" fn efi_main(_handle: uefi::Handle, system_table: SystemTable<
         .unwrap()
         .get();
 
+    let _ = system_table.stdout().write_str("Ficheros en la particion EFI: \n");
+
     let mut root = unsafe { (*fs).open_volume().expect("Could not open volume").unwrap() };
-
-    let mut buf = Vec::<u8>::with_capacity(FileInfo::alignment() * 50);
-    let root_file = loop {
-        match root.read_entry(&mut buf[..]).map_err(|err| err.split()) {
-            Ok(ret) => break ret.log().unwrap(),
-            Err((_, Some(new_size))) => {
-                buf.extend((0..new_size - buf.len()).map(|_| 0));
-            }
-            Err((status, None)) => panic!("Can't read root dir status: {:?}", status),
-        };
-    };
-
-    let _ = system_table.stdout().write_str("Ficheros en la particion EFI: /");
-    let _ = system_table.stdout().write_str(String::from_utf16_lossy(root_file.file_name().to_u16_slice()).as_str());
-    let _ = system_table.stdout().write_str("/\n");
+    crawl_tree(&system_table, &mut root, 0);
 
     loop {}
 }
